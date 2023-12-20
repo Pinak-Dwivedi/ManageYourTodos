d8065a9bb83be8f2f56260ba7f16ca6e214b9b4f
Pinak <pinak@git.com>
2023-12-20 16:49:39 +0530

    updated hero and mental-peace sections

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,24 +1,41 @@
 import "./Home.css";
-import {Navigate} from "react-router-dom";
+import { Navigate } from "react-router-dom";
 import { useContext } from "react";
 import AuthContext from "../../index";
+import heroImage from "../../assets/images/hero-image.jpg";
+import mentalPeaceImage from "../../assets/images/mental-peace-image.jpg";
 
-export default function Home(){
+export default function Home() {
+  const { isAuthenticated } = useContext(AuthContext);
 
-    const {isAuthenticated} = useContext(AuthContext);
-    
-    if(isAuthenticated)
-    return <Navigate to="/dashboard" />
+  if (isAuthenticated) return <Navigate to="/dashboard" />;
 
-    return(
-        <div className="home-container">
-            <section className="home-hero">
+  return (
+    <div className="home-container">
+      <section className="home-hero">
+        <h2 className="hero-title">Manage Your Todos</h2>
+
+        <img
+          src={heroImage}
+          className="hero-image"
+          alt="hero section - getting things done"
+        />
+      </section>
+
+      <section className="mental-peace-section">
+        <h2 className="mental-peace-title">
+          Achieve mental peace with
+          <span className="mental-peace-title--highlighted">
             Manage Your Todos
-            </section>
+          </span>
+        </h2>
 
-            <section className="mental-peace-section">
-                Achieve mental peace with <span className="highlighted">Manage Your Todos</span>
-            </section>
-        </div>
-    )
+        <img
+          src={mentalPeaceImage}
+          className="mental-peace-image"
+          alt="mental peace section - peace"
+        />
+      </section>
+    </div>
+  );
 }
